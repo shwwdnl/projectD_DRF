2cf8897c6d4a6d4a293b5a97b33cc34ff319bd6c
user <shwwdnl@gmail.com>
2024-02-10 16:57:57 +0500

    update file

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'users',
     'main',
+    'django-filter',
 ]
 
 MIDDLEWARE = [
--- main/serializers.py
+++ main/serializers.py
@@ -4,12 +4,22 @@ from rest_framework.relations import SlugRelatedField
 from main.models import Course, Lesson
 from users.models import User
 
+class LessonListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = ['lesson_name', 'lesson_description']
+
 
 class CourseSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.IntegerField(source='lesson_set.count', read_only=True)
+    lessons = serializers.SerializerMethodField()
 
     class Meta:
         model = Course
         fields = '__all__'
+
+    def get_lessons(self, course):
+        return LessonListSerializer(Lesson.objects.filter(course=course), many=True).data
 
 
 class LessonSerializer(serializers.ModelSerializer):
@@ -17,4 +27,5 @@ class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+
 
